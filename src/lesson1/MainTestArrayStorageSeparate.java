3c57ad65599f40375a2df1bbe2e1db65304410e8
gagarina6794 <1331gagarina>
2018-12-21 21:23:17 +0200

    Test save lesson 1 add check null

--- src/lesson1/MainTestArrayStorageSeparate.java
+++ src/lesson1/MainTestArrayStorageSeparate.java
@@ -1,20 +1,20 @@
 package lesson1;
 
-import java.util.Arrays;
-
 public class MainTestArrayStorageSeparate {
     private static final ArrayStorageImpl TEST_ARRAY_STORAGE = new ArrayStorageImpl();
 
     private static final int SIZE = 5;
+    private static boolean error;
 
-    private static String colorRed = "\u001B[31m";
-    private static String colorGreen = "\u001B[32m";
-    private static String colorBlack = "\u001B[30m";
+    private static String red = "\u001B[31m";
+    private static String green = "\u001B[32m";
+    private static String black = "\u001B[30m";
 
     private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
         testSave();
+
         System.out.println("TEST REPORT");
         System.out.println("=======================================");
         System.out.println(sb.toString());
@@ -24,44 +24,41 @@ public class MainTestArrayStorageSeparate {
     private static void testSave() {
         Resume[] testStorage = fillData();
         clearData();
+        Resume[] storage = TEST_ARRAY_STORAGE.storage;
         int sizeStorage = 0;
-        int countMistakes = 0;
-        final Resume[] storage = TEST_ARRAY_STORAGE.storage;
 
         for (int i = 0; i < SIZE; i++) {
             Resume resume = new Resume();
             resume.uuid = "uuid_" + i;
             TEST_ARRAY_STORAGE.save(resume);
 
-            if (!testStorage[i].uuid.equals(storage[i].uuid)) {
-                formatMessages("Test save was complete with error - RESUME NOT EXIST!", colorRed);
-                countMistakes++;
+            if (storage[i] == null || !storage[i].uuid.equals(testStorage[i].uuid)) {
+                formatMessages("Test save was complete with error - RESUME NOT EXIST!", red);
                 break;
             } else {
                 sizeStorage++;
             }
 
             if (sizeStorage != TEST_ARRAY_STORAGE.size) {
-                formatMessages("Test save was complete with error - SIZE!", colorRed);
-                countMistakes++;
+                formatMessages("Test save was complete with error - SIZE!", red);
                 break;
             }
         }
 
         for (int i = SIZE; i < storage.length; i++) {
             if (storage[i] != null) {
-                formatMessages("Test save was complete with error!", colorRed);
-                countMistakes++;
+                formatMessages("Test save was complete with error!", red);
                 break;
             }
         }
 
-        if (countMistakes == 0) {
-            formatMessages("Test save was complete successful!", colorGreen);
+        if (!error) {
+            formatMessages("Test save was complete successful!", green);
         }
     }
 
     private static void beforeTest() {
+        error = false;
         clearData();
         fillData();
     }
@@ -80,10 +77,7 @@ public class MainTestArrayStorageSeparate {
     }
 
     private static void formatMessages(String message, String color) {
-        if (color.equals(colorGreen)) {
-            sb.append(color).append(message).append(colorBlack).append("\n");
-        } else {
-            sb.append(color).append(message).append(colorBlack).append("\n");
-        }
+        sb.append(color).append(message).append(black).append("\n");
+        error = true;
     }
 }
