b01bd569be43b535be1cadb2546b2a962b84673e
gagarina6794 <1331gagarina>
2018-12-22 07:53:46 +0200

    complete all tests

--- src/lesson1/MainTestArrayStorageSeparate.java
+++ src/lesson1/MainTestArrayStorageSeparate.java
@@ -2,18 +2,19 @@ package lesson1;
 
 public class MainTestArrayStorageSeparate {
     private static final ArrayStorageImpl TEST_ARRAY_STORAGE = new ArrayStorageImpl();
-
     private static final int SIZE = 5;
     private static boolean error;
-
     private static String red = "\u001B[31m";
     private static String green = "\u001B[32m";
-    private static String black = "\u001B[30m";
-
     private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
         testSave();
+        testGet();
+        testDelete();
+        testClear();
+        testGetAll();
+        testSize();
 
         System.out.println("TEST REPORT");
         System.out.println("=======================================");
@@ -25,7 +26,7 @@ public class MainTestArrayStorageSeparate {
         Resume[] testStorage = fillData();
         clearData();
         Resume[] storage = TEST_ARRAY_STORAGE.storage;
-        int sizeStorage = 0;
+        int sizeTestStorage = 0;
 
         for (int i = 0; i < SIZE; i++) {
             Resume resume = new Resume();
@@ -36,10 +37,10 @@ public class MainTestArrayStorageSeparate {
                 formatMessages("Test save was complete with error - RESUME NOT EXIST!", red);
                 break;
             } else {
-                sizeStorage++;
+                sizeTestStorage++;
             }
 
-            if (sizeStorage != TEST_ARRAY_STORAGE.size) {
+            if (sizeTestStorage != TEST_ARRAY_STORAGE.size) {
                 formatMessages("Test save was complete with error - SIZE!", red);
                 break;
             }
@@ -53,12 +54,111 @@ public class MainTestArrayStorageSeparate {
         }
 
         if (!error) {
-            formatMessages("Test save was complete successful!", green);
+            formatMessages("1: Test save was complete successful!", green);
+        }
+    }
+
+    private static void testGet() {
+        beforeTest();
+
+        for (int i = 0; i < SIZE; i++) {
+            Resume resume = TEST_ARRAY_STORAGE.get("uuid_" + i);
+            if (resume == null || !resume.uuid.equals("uuid_" + i)) {
+                formatMessages("Test get was complete with not exist error!", red);
+                break;
+            }
+        }
+
+        if (!error) {
+            formatMessages("2: Test get was complete successful!", green);
+        }
+    }
+
+    private static void testDelete() {
+        beforeTest();
+
+        for (int i = 0; i < SIZE; i++) {
+            try {
+                TEST_ARRAY_STORAGE.delete("uuid_" + i);
+            } catch (NullPointerException e) {
+                formatMessages("Test delete was complete with error!", red);
+                break;
+            }
+        }
+
+        for (int i = 0; i < TEST_ARRAY_STORAGE.storage.length; i++) {
+            if (TEST_ARRAY_STORAGE.storage[i] != null) {
+                formatMessages("Test delete was complete with error - RESUME WAS NOT DELETED!", red);
+                break;
+            }
+        }
+
+        if (TEST_ARRAY_STORAGE.size != 0) {
+            formatMessages("Test delete was complete with error - SIZE!", red);
+        }
+
+        if (!error) {
+            formatMessages("3: Test delete was complete successful!", green);
+        }
+    }
+
+    private static void testClear() {
+        beforeTest();
+        TEST_ARRAY_STORAGE.clear();
+
+        for (int i = 0; i < TEST_ARRAY_STORAGE.storage.length; i++) {
+            if (TEST_ARRAY_STORAGE.storage[i] != null) {
+                formatMessages("Test clear was complete with error - STORAGE CONTAINS RESUMES!", red);
+                break;
+            }
+        }
+
+        if (TEST_ARRAY_STORAGE.size != 0) {
+            formatMessages("Test clear was complete with error - SIZE!", red);
+        }
+
+        if (!error) {
+            formatMessages("4: Test clear was complete successful!", green);
+        }
+    }
+
+    private static void testGetAll() {
+        beforeTest();
+        Resume[] allResumes = TEST_ARRAY_STORAGE.getAll();
+        Resume[] testResumes = TEST_ARRAY_STORAGE.storage;
+
+        if (allResumes.length != SIZE) {
+            formatMessages("Test getAll was complete with error - SIZE!", red);
+            return;
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            if (allResumes[i].uuid == null || !allResumes[i].uuid.equals(testResumes[i].uuid)) {
+                formatMessages("Test getAll was complete with error!", red);
+                break;
+            }
+        }
+
+        if (!error) {
+            formatMessages("5: Test getAll was complete successful!", green);
+        }
+    }
+
+    private static void testSize() {
+        beforeTest();
+
+        if (TEST_ARRAY_STORAGE.size() != SIZE) {
+            formatMessages("Test size was complete with error - SIZE!", red);
+        }
+
+        if (!error) {
+            formatMessages("6: Test size was complete successful!", green);
         }
     }
 
     private static void beforeTest() {
         error = false;
+        TEST_ARRAY_STORAGE.size = SIZE;
         clearData();
         fillData();
     }
@@ -73,10 +173,13 @@ public class MainTestArrayStorageSeparate {
     }
 
     private static void clearData() {
-        TEST_ARRAY_STORAGE.storage = new Resume[10_000];
+        for (int i = 0; i < TEST_ARRAY_STORAGE.storage.length; i++) {
+            TEST_ARRAY_STORAGE.storage[i] = null;
+        }
     }
 
     private static void formatMessages(String message, String color) {
+        String black = "\u001B[30m";
         sb.append(color).append(message).append(black).append("\n");
         error = true;
     }
